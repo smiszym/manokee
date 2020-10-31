028e410b0680189ced385e697b07e93f8f1395d0
Michał Szymański <smiszym@gmail.com>
2020-10-31 22:30:25 +0100

    Add format_seconds() and parse_seconds()

--- manokee/time_formatting.py
+++ manokee/time_formatting.py
@@ -5,10 +5,7 @@ import re
 from typing import Optional
 
 
-def format_frame(amio_interface: Interface, frame: int) -> str:
-    if amio_interface is None:
-        return f"frame {frame}"
-    seconds = amio_interface.frame_to_secs(frame)
+def format_seconds(seconds: float) -> str:
     full_seconds = int(seconds)
     minutes = full_seconds // 60
     full_seconds = full_seconds % 60
@@ -16,20 +13,29 @@ def format_frame(amio_interface: Interface, frame: int) -> str:
     return f"{minutes}:{full_seconds:02d}.{tenths}"
 
 
+def format_frame(amio_interface: Interface, frame: int) -> str:
+    if amio_interface is None:
+        return f"frame {frame}"
+    return format_seconds(amio_interface.frame_to_secs(frame))
+
+
 time_format_pattern = re.compile(r"\s*([0-9]+):([0-9]+)\.([0-9])\s*")
 
 
-def parse_frame(amio_interface: Interface, formatted: str) -> Optional[int]:
-    if amio_interface is None:
-        return None
+def parse_seconds(formatted: str) -> Optional[float]:
     m = time_format_pattern.match(formatted)
     if m is None:
         return None
     min = int(m.group(1))
     sec = int(m.group(2))
     tenths = int(m.group(3))
-    seconds = 60 * min + sec + 0.1 * tenths
-    return amio_interface.secs_to_frame(seconds)
+    return 60 * min + sec + 0.1 * tenths
+
+
+def parse_frame(amio_interface: Interface, formatted: str) -> Optional[int]:
+    if amio_interface is None:
+        return None
+    return amio_interface.secs_to_frame(parse_seconds(formatted))
 
 
 def format_beat(
