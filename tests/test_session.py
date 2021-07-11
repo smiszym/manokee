7d829da7c525c20c877eb580f025f61755dde928
Michał Szymański <smiszym@gmail.com>
2021-07-11 13:41:17 +0200

    Move test asset

--- tests/test_session.py
+++ tests/test_session.py
@@ -4,7 +4,7 @@ import pytest
 
 def test_load_session():
     # TODO Make it possible to open a session without specifying frame rate
-    session = Session(48000, "tests/assets/simple.mnk")
+    session = Session(48000, "tests/assets/sessions/simple/session.mnk")
     assert session.bpm == 154
     assert sum(1 for _ in session.tracks) == 2
     assert sum(1 for _ in session.marks) == 0
