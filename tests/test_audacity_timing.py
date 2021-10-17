63ccb96da2c0629b5dc28c80449a8efe1dd7ddfd
Michał Szymański <smiszym@gmail.com>
2021-10-17 15:05:20 +0200

    Rewrite test_audacity_timing.py to work on a real .aup file

--- tests/test_audacity_timing.py
+++ tests/test_audacity_timing.py
@@ -1,25 +1,32 @@
-from unittest.mock import Mock
-
+from manokee.audacity.project import parse as parse_aup
 from manokee.timing.audacity_timing import AudacityTiming
 
 
 def test_audacity_timing():
-    label_track = Mock()
-    label_track.get_name = Mock(return_value="ofs=0")
-    label_track.get_label_positions = Mock(return_value=[1.0, 3.0, 5.0])
-    project = Mock()
-    project.get_label_track = Mock(return_value=label_track)
+    project = parse_aup("tests/assets/audacity-projects/simple.aup")
     timing = AudacityTiming(project)
 
-    assert timing.seconds_to_beat(-1.0) == -1
-    assert timing.seconds_to_beat(1.0) == 0
-    assert timing.seconds_to_beat(3.0) == 1
-    assert timing.seconds_to_beat(5.0) == 2
-    assert timing.seconds_to_beat(7.0) == 3
+    assert timing.average_beat_length == 2.375
 
-    assert timing.beat_to_seconds(-1) == -1.0
-    assert timing.beat_to_seconds(0) == 1.0
-    assert timing.beat_to_seconds(1) == 3.0
-    assert timing.beat_to_seconds(2) == 5.0
-    assert timing.beat_to_seconds(3) == 7.0
-    assert timing.beat_to_seconds(4) == 9.0
+    offset = 0.1
+
+    # TODO: Extrapolate in seconds_to_beat
+    # assert timing.seconds_to_beat(-3.75 - offset) == -2
+    # assert timing.seconds_to_beat(-1.375 - offset) == -1
+    assert timing.seconds_to_beat(1 - offset) == 0
+    assert timing.seconds_to_beat(3 - offset) == 1
+    assert timing.seconds_to_beat(5 - offset) == 2
+    assert timing.seconds_to_beat(7.5 - offset) == 3
+    assert timing.seconds_to_beat(10 - offset) == 4
+    # assert timing.seconds_to_beat(22.375 - offset) == 9
+    # assert timing.seconds_to_beat(24.75 - offset) == 10
+
+    assert timing.beat_to_seconds(-2) == -3.75 - offset
+    assert timing.beat_to_seconds(-1) == -1.375 - offset
+    assert timing.beat_to_seconds(0) == 1 - offset
+    assert timing.beat_to_seconds(1) == 3 - offset
+    assert timing.beat_to_seconds(2) == 5 - offset
+    assert timing.beat_to_seconds(3) == 7.5 - offset
+    assert timing.beat_to_seconds(4) == 10 - offset
+    assert timing.beat_to_seconds(9) == 22.375 - offset
+    assert timing.beat_to_seconds(10) == 24.75 - offset
